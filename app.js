4f04845770818a38858e3354625556ea598bd28f
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2015-10-04 17:42:24 -0300

    FCC zipline - Intermediate Front-ent projects. Camper news

---
+++ app.js
@@ -0,0 +1,7 @@
+(function() {
+
+	var app = angular.module( 
+		'stories', [ 'story-modules' ]
+	);
+	
+})();
---
+++ stories.js
@@ -0,0 +1,173 @@
+(function() {
+	
+	var app = angular.module( 'story-modules', [] );
+
+	app.controller( 
+		'StoriesGridController', 
+		function( loadStoriesFactory ) {
+			var self 		= this;
+			this.stories 	= loadStoriesFactory.stories;
+			loadStoriesFactory.loadStories();
+		}
+	);
+	app.controller( 
+		'CamperInfoController', 
+		function( $scope, StateHolderFactory ) {
+			var self = this;
+
+			this.hasData = false;
+
+			this.focusedStory = {
+				username 	: null,
+				points 		: null,
+				date 		: null
+			}
+			$scope.focusedStory = this.focusedStory;
+
+			this.updateStory = function( val ) {
+				this.focusedStory.username 	= val.author.username;
+				this.focusedStory.points 	= val.rank;
+				this.focusedStory.date 		= val.timePosted;
+				this.focusedStory.campUser 	= val.campUser;
+				this.hasData 				= true;
+				$scope.$apply();
+			}
+
+			StateHolderFactory.addCallback(
+				function() {
+					self.updateStory( StateHolderFactory.focusedData );
+				}
+			);
+		}
+	);
+
+	app.constant( 
+		'endpoint', 
+		'http://www.freecodecamp.com/news/hot' 
+	);
+	app.constant(
+		'basePath',
+		'http://www.freecodecamp.com/'
+	);
+	app.factory( 
+		'loadStoriesFactory', 
+		function( endpoint, basePath, $http ) {
+
+			var stories 		= []
+			var loadStories 	= function( ) {
+				$http.get( endpoint ).success(function( data ) {
+					data.forEach( function( curr ) {
+						curr[ 'campUser' ] = basePath + curr.author.username;
+						stories.push( curr );
+					});
+				});
+			};
+			return {
+				stories 		: stories,
+				loadStories 	: loadStories
+			};
+		}
+	);
+	app.factory(
+		'StateHolderFactory',
+		function( ) {
+			return {
+				focusedData : null,
+				callbacks : [],
+				updatedFocused : function( focused ) {
+					this.focusedData = focused;
+					this.notifyObservers();
+				},
+				addCallback : function( c ) {
+					this.callbacks.push( c );
+				},
+				notifyObservers : function() {
+					this.callbacks.forEach( function( curr ) {
+						curr( );
+					});
+				}
+			};
+		}
+	);
+
+	app.directive( 
+		'storiesHeader', 
+		function( loadStoriesFactory ) {
+			return {
+				restrict		: 'E',
+				templateUrl		: 'stories-header.html',
+				link			: function( scope, element, attrs ) {
+					element.click( function() {
+						$('html, body').animate( 
+							{ 'scrollTop' : 0 } , 500
+						);
+						loadStoriesFactory.loadStories();
+					});
+				}
+			}
+		}
+	);
+	app.directive('storiesGrid', function() {
+		return {
+			restrict		: 'E',
+			templateUrl		: 'stories-grid.html',
+			controller		: 'StoriesGridController',
+			controllerAs	: 'grid'
+		}
+	});
+	app.directive('storySingle', function( ) {
+		return {
+			restrict		: 'E',
+			templateUrl		: 'story-single.html',
+			controller 		: function( 
+				$scope, 
+				StateHolderFactory 
+			) {
+				this.current 		= $scope.story;
+				this.updateShared 	= function( ) {
+					StateHolderFactory.updatedFocused(
+						this.current 
+					);
+				}
+			},
+			controllerAs 	: "single",
+			link 			: function( scope, element, attrs, controller ) {
+				element.hover(
+					function( ) {
+						if ( ! window.matchMedia( '(max-width:899px)' ).matches ) {
+							controller.updateShared();
+							$( 'div.meta-info' ).stop( true, true );
+							$( 'div.meta-info' ).fadeTo( 100, 1 );
+						}
+					},
+					function( ){
+						if ( ! window.matchMedia( '(max-width:899px)' ).matches ) {
+							$( 'div.meta-info' ).stop( true, true );
+							$( 'div.meta-info' ).fadeTo( 100, 0.4 );
+						}
+					}
+				);
+			}
+		}
+	});
+	app.directive( 'camperSideInfo', function() {
+		return {
+			restrict 		: 'E',
+			templateUrl 	: 'story-side-info.html',
+			controller 		: 'CamperInfoController',
+			controllerAs	: 'info',
+			link 			: function( scope, element ) {
+				$( 'div.meta-info' ).fadeTo( 0, 0.1 );
+				$( 'div.meta-info' ).hover( function() {
+					$( 'div.meta-info' ).stop( true, true );
+					$( 'div.meta-info' ).fadeTo( 100, 1 );
+				}, function() {
+					$( 'div.meta-info' ).stop( true, true );
+					$( 'div.meta-info' ).fadeTo( 100, 0.4 );
+				});
+			}
+		}
+	});
+
+})();
+
