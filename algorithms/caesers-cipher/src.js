ddb3439db8254c30ff7f9b177f883081b36a8808
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-06-25 12:39:41 -0300

    Adding Code for Rot13 decode function.

--- algorithms/caesers-cipher/src.js
+++ algorithms/caesers-cipher/src.js
@@ -1,5 +1,39 @@
 
 
-function decode(plainText) {
-  
+function Rot13() {
+
+  this.decode = function(plainText) {
+
+    if (!stringIsValid(plainText)) {
+      return null;
+    }
+
+    return plainText.toUpperCase().split('').map(function(currentChar) {
+      if (currentChar.match('^[a-zA-Z]+$') == null) {
+        return currentChar;
+      }
+
+      return String.fromCharCode(calculateCode(currentChar));
+    }).reduce(function(previousValue, currentChar) {
+      return previousValue + currentChar;
+    });
+  }
+
+  function stringIsValid(text) {
+    return typeof text === 'string' && text.length !== 0;
+  }
+
+  function calculateCode(char) {
+    var firstUpperCode = 'A'.charCodeAt(0);
+    var lastUpperCode = 'Z'.charCodeAt(0) + 1;
+
+    var decoded = char.charCodeAt(0) - 13;
+
+    if (firstUpperCode > decoded) {
+      decoded = lastUpperCode - (firstUpperCode - decoded);
+    }
+
+    return decoded;
+  }
+
 }
