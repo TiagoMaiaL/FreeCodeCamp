6c6f68bef9f3e72841cd11be9352d956d32cd638
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-07-11 13:12:23 -0300

    Initial Game structure.

---
+++ front-end/advanced-projects/simon/main.js
@@ -0,0 +1,90 @@
+
+/**
+ * Bootstrap
+ * @return void
+ */
+$(document).ready(function() {
+
+  /**
+   * Game control handler.
+   */
+  $('#game-control').on('click', function() {
+    // TODO: Instantiate a simon controller instance
+    // TODO: Check whether the instance exists before instantiating
+    // TODO: Start the game.
+  });
+
+});
+
+/**
+ * Object responsible for controlling
+ * main game events and actions.
+ */
+function SimonController() {
+
+  /**
+   * Number of colors in the current sequence.
+   * @type Number
+   */
+  var sequenceCount = 0;
+
+  /**
+   * Current game sequence.
+   * @type SequenceHandler
+   */
+  var sequence = null;
+
+  /**
+   * Starts the game.
+   * @return void
+   */
+  this.start = function() {
+    
+  }
+
+  /**
+   * Pauses the game.
+   * @return void
+   */
+  this.stop = function() {
+
+  }
+
+}
+
+/**
+ * Object responsible for handling user
+ * input through the game buttons.
+ */
+function ControlHandler() {
+  // ...
+}
+
+/**
+ * Object responsible for controlling
+ * the sequence of the game
+ */
+function SenquenceHandler() {
+  // ...
+}
+
+/**
+ * Object responsible for presenting a given sequence.
+ */
+function SequencePresenter() {
+  // ...
+}
+
+/**
+ * Object representing a sequence of colors.
+ */
+function Sequence() {
+  // ...
+}
+
+/**
+ * Object representing a given color.
+ */
+function Color() {
+  // ...
+}
