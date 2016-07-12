e91746bc11d26ebc83d7267c22930378abca53cb
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-07-12 09:59:05 -0300

    Initial test suite for SequenceHandler object

--- front-end/advanced-projects/simon/specs/SequenceHandler.js
+++ front-end/advanced-projects/simon/specs/SequenceHandler.js
@@ -7,6 +7,28 @@
  * https://www.freecodecamp.com/challenges/build-a-simon-game
  */
 
-describe("SequenceHandler", function() {
+describe('SequenceHandler', function() {
+
+  var handler;
+
+  beforeEach(function() {
+    handler = new SequenceHandler();
+  });
+
+  it('should generate a color object', function() {
+    expect(handler.getRandomColor()).toEqual(jasmine.any(Color));
+  });
+
+  it('should append a color to the passed sequence', function() {
+    var sequence = handler.addNextColor(new Sequence());
+    expect(sequence.count()).toBe(1);
+    expect(sequence.getElement(1)).toEqual(jasmine.any(Color));
+  });
+
+  it('should deny non sequence values passed as parameter', function() {
+    expect(function() {
+      handler.addNextColor(null);
+    }).toThrow('Invalid sequence passed as parameter');
+  });
 
 });
