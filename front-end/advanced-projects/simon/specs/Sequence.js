27bcac04e7c37de6d0746bb46574a059de410b39
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-07-13 10:07:13 -0300

    Adding initial code for the sequence spec

---
+++ front-end/advanced-projects/simon/specs/Sequence.js
@@ -0,0 +1,44 @@
+/**
+ * Free Code Camp
+ *
+ * Advanced Front-End Projects.
+ *
+ * Simon game solution.
+ * https://www.freecodecamp.com/challenges/build-a-simon-game
+ */
+
+describe('Sequence', function() {
+
+  /**
+   * Convenience method for getting a Sequence object.
+   * @param  Array items
+   * @return Sequence
+   */
+  function sequenceFactory(items) {
+    return new Sequence(array);
+  }
+
+  it('should get an array as items', function() {
+    expect(sequenceFactory([1, 2, 3]).getItems()).toBe([1, 2, 3]);
+  });
+
+  it('should return the sencond element', function() {
+    expect(sequenceFactory([1, 2, 3]).getElement(1)).toBe(2);
+  });
+
+  it('should return the length of the sequence', function() {
+    expect(sequenceFactory([1, 2, 3]).count()).toBe(3);
+  });
+
+  it('should add a new element into the sequence', function() {
+    expect(sequenceFactory([1, 2, 3]).push(4).getElement(3)).toBe(4);
+  });
+
+  it('should compare two different sequences', function() {
+    var firstSequence = sequenceFactory([1, 2, 3]);
+    var secondSequence = sequenceFactory([1, 2, 3]);
+
+    expect(firstSequence.equals(secondSequence)).toBeTruthy();
+  });
+  
+});
