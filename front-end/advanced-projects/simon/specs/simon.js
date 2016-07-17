618555a1f8e623752deaa0492aac8da68a4e9ba9
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-07-17 11:57:29 -0300

    Adding initial tests for number of rounds and validation of round

--- front-end/advanced-projects/simon/specs/simon.js
+++ front-end/advanced-projects/simon/specs/simon.js
@@ -14,7 +14,7 @@ describe('Simon', function() {
 
   beforeEach(function() {
     game = new Simon();
-    presenterSpy = jasmine.createSpyObj('SequencePresenter', ['present']);
+    presenterSpy = jasmine.createSpyObj('GamePresenter', ['present']);
     game.setPresenter(
       presenterSpy
     );
@@ -95,7 +95,7 @@ describe('Simon', function() {
   });
 
   it('should set and provide a presentation object', function() {
-    var presenter = jasmine.createSpyObj('SequencePresenter', ['present']);
+    var presenter = jasmine.createSpyObj('GamePresenter', ['present']);
     game.setPresenter(presenter);
     expect(game.getPresenter()).toBe(presenter);
   });
@@ -117,6 +117,45 @@ describe('Simon', function() {
     game.goNextRound();
 
     expect(player.getColorsCount()).toBe(0);
+  });
+
+  it('should have a setter and getter for maxRoundsNumber', function() {
+    var maxRoundsNumber = 3;
+    game.setMaxRounds(maxRoundsNumber);
+    expect(game.getMaxRounds()).toBe(3);
+  });
+
+  // TODO: write this test in a better way.
+  it('should present the winner if sequences are final and equal', function() {
+    var maxRoundsNumber = 2;
+    var presenter = jasmine.createSpyObj(
+      'GamePresenter',
+      ['present', 'presentWinner']
+    );
+    game.setMaxRounds(maxRoundsNumber);
+    game.setPresenter(presenter);
+
+    var gameColors = new Sequence([new Color('red'), new Color('blue')]);
+
+    game.getPlayer().resetColors();
+    game.getPlayer().addColor(new Color('red'));
+    game.getPlayer().addColor(new Color('blue'));
+    game.setSequence(gameColors);
+
+    game.validatePlayerRound();
+
+    expect(presenter.presentWinner).toHaveBeenCalled();
+  });
+
+  it('should go to next round after validating the previous', function() {
+    game.setMaxRounds(2);
+    game.getPlayer().addColor(new Color('red'));
+    game.setSequence(new Sequence([new Color('red')]));
+
+    game.validatePlayerRound();
+
+    expect(game.getPresenter().present).toHaveBeenCalled();
+    expect(game.getPlayer().getColorsCount()).toBe(0);
   });
 
 });
