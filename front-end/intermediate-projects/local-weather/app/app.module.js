d5e828ec0ba98e4780a4ffc4dacba2137cea94dd
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-08-25 01:32:36 -0300

    Adding ngRoute and resource. Creating app module

---
+++ front-end/intermediate-projects/local-weather/app/app.module.js
@@ -0,0 +1,10 @@
+/**
+ * Free Code Camp
+ *
+ * Intermediate Front-End Projects.
+ *
+ * Show The Local Weather solution.
+ * https://www.freecodecamp.com/challenges/show-the-local-weather
+ */
+
+angular.module('WeatherApp', ['ngRoute', 'ngResource']);
